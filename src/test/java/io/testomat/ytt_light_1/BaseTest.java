42146cd164aa0fe04eec7babf43b2d1fb0c91c93
Yana Ivchenko <zalanis7@gmail.com>
2025-05-26 13:22:06 +0300

    Small refactor of the firstTest Added new test anotherTest for name project search

---
+++ src/test/java/io/testomat/ytt_light_1/BaseTest.java
@@ -0,0 +1,7 @@
+package io.testomat.ytt_light_1;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class BaseTest {
+    public static Dotenv env = Dotenv.load();
+}
--- src/test/java/io/testomat/ytt_light_1/ProjectPageTests.java
+++ src/test/java/io/testomat/ytt_light_1/ProjectPageTests.java
@@ -1,32 +1,89 @@
 package io.testomat.ytt_light_1;
 
+import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
-public class ProjectPageTests {
+public class ProjectPageTests extends BaseTest{
+
+    static String baseUrl = env.get("BASE_URL");
+    static String email = env.get("EMAIL");
+    static String password = env.get("PASSWORD");
+    String targetProjectName = "manufacture light";
+
+    @BeforeAll
+    static void openTestomatAndLogin(){
+        open(baseUrl);
+
+        loginUser(email, password);
+    }
+
+    @BeforeEach
+    void openHomePage(){
+        open(baseUrl);
+    }
+
 
     @Test
     void firstTest() {
-        open("https://app.testomat.io/");
 
-        //user login
-        $("#content-desktop #user_email").setValue("zalanis7@gmail.com");
-        $("#content-desktop #user_password").setValue("Mattinazoria2409");
+        searchForProject(targetProjectName);
+
+        selectProject(targetProjectName);
+
+        waitingForProjectToLoad(targetProjectName);
+    }
+
+    @Test
+    public void anotherTest() {
+
+        searchForProject(targetProjectName);
+
+        SelenideElement targetProject = countOfProjectsShouldBeEqualTo(2).first();
+
+        countOfTestCasesShouldBeEqualTo(targetProject, 0);
+    }
+
+    private static void waitingForProjectToLoad(String targetProjectName) {
+        $(".first h2").shouldHave(Condition.text(targetProjectName));
+        $(".first [href*='/readme']").shouldHave(Condition.text("Readme"));
+    }
+
+    private static void selectProject(String targetProjectName) {
+        $(byText(targetProjectName)).click();
+    }
+
+    private static void searchForProject(String targetProjectName) {
+        $("#search").setValue(targetProjectName);
+    }
+
+    private static void countOfTestCasesShouldBeEqualTo(SelenideElement targetProject, int expectedCount) {
+        String countOfTests = targetProject.$("p").getText();
+        String digitText = countOfTests.replaceAll("\\D+","" );
+        Integer actualCountOfTests = Integer.parseInt(digitText);
+        Assertions.assertEquals(0, actualCountOfTests);
+    }
+
+    @NotNull
+    private static ElementsCollection countOfProjectsShouldBeEqualTo(int expectedSize) {
+        return $$("#grid ul li").filter(visible).shouldHave(CollectionCondition.size(expectedSize));
+    }
+
+    public static void loginUser(String email, String password) {
+        $("#content-desktop #user_email").setValue(email);
+        $("#content-desktop #user_password").setValue(password);
         $("#content-desktop #user_remember_me").click();
         $("#content-desktop [name=\"commit\"]").click();
-        $(".common-flash-success").shouldBe(Condition.visible);
-
-        //project search
-        $("#search").setValue("manufacture light");
-
-        //project select
-        $(byText("manufacture light")).click();
-
-        //waiting for project to load
-        $("h2").shouldHave(Condition.text("manufacture light"));
+        $(".common-flash-success").shouldBe(visible);
     }
 }
